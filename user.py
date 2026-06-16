355b5f0d81b54dc7231c3079dd42604536c0ec03
Emilien-Marot <12394@holbertonstudents.com>
2026-06-16 09:18:00 +0400

    test commit

--- user.py
+++ user.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """
-This is a module-level docstring.
-It describes the purpose of the entire script/module.
+Defines the user profile to be stored in the database
 """
 
 
@@ -14,15 +13,22 @@ Base = declarative_base()
 
 class User(Base):
     '''
-    Docstring for the class
+    Class representing a user of the app
     '''
-    __tablename__ = 'states'
+    __tablename__ = 'users'
     id = Column(Integer, nullable=False, primary_key=True)
     name = Column(String(128), nullable=True)
     password = Column(Integer, nullable=True)
     coach = Column(Boolean, nullable=False)
 
     def __init__(self, name=None, pwd=None, coach = False):
+        '''
+        generates a new profile
+
+        :param name: name of the user (None for guests)
+        :param pwd: user's password (None for guests)
+        :param coach: is the user a coach (false for clients and guests)
+        '''
         if name == None:
             self.name = None
             self.password = None
@@ -37,6 +43,13 @@ class User(Base):
         self.coach = coach
     
     def register(self, name, pwd):
+        '''
+        register the profile of a guest
+
+        :param name: name of the user
+        :param pwd: user's password
+        :return:
+        '''
         if name == None:
             raise Exception("name empty")
         if pwd == None:
@@ -45,4 +58,10 @@ class User(Base):
         self.password = generate_password_hash(pwd)
 
     def verify_pwd(self, pwd):
+        '''
+        verify the password
+
+        :param pwd: password to be checked
+        :return: True if the hash of the password is the same as the profile
+        '''
         return check_password_hash(self.password, pwd)
